b90aff73eafbd4c0839f7bb996731c69526b24c7
Andika <litageming@gmail.com>
2023-03-28 14:22:27 +0700

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+// mengambil elemen button
+const tweetButton = document.querySelector('button');
+
+// menambahkan event listener ketika button di klik
+tweetButton.addEventListener('click', function() {
+  alert('Anda berhasil tweet!');
+});
